1b64d469a7d22f5c205554e67458b0935fe7dba5
Mário Hunka <hunka.mario@gmail.com>
2019-12-01 13:47:48 +0100

    change theme color.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0A0A0A',
+        theme_color: '#0A0A0A',
         display: 'minimal-ui',
         icon: 'src/images/logo_black_white.png', // This path is relative to the root of the site.
       },
